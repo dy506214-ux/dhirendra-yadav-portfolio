7cac8e0b235190a9fa5cf2ae7bd452e7a53d878e
DHIRENDRA YADAV <dy506214@gmail.com>
2026-07-03 22:02:55 +0530

    feat: load DATABASE_URL from .env file into process.env in scratch.js

--- scratch.js
+++ scratch.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
+const env = fs.readFileSync('.env', 'utf8');
+const match = env.match(/DATABASE_URL="?([^"\n\r]+)"?/);
+if (match) {
+  process.env.DATABASE_URL = match[1];
+}
+
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 async function main() {
